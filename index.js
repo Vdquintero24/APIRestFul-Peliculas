9bc7bb0b549103aa0a9948b1828b8d5c1af051d0
vd.quintero24 <vd.quintero24@info.uas.edu.mx>
2026-05-29 15:30:27 -0700

    cambiando a node 20

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 app.use(express.json());
 
